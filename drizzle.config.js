a9c0258e530508b1b00ccbe6e838068e1b4b026c
Harshal Presswala <141266067+Harshal-0901@users.noreply.github.com>
2024-07-18 20:07:23 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,7 @@ export default {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://quill_owner:OG6YsdAb0IUu@ep-shrill-sun-a1ovipnh.ap-southeast-1.aws.neon.tech/AI-Content-Generator?sslmode=require',
+      url: '******',
     }
   };
   
